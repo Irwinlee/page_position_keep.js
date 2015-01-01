ae001c0b7defb667e6ff00768fc484fea58a01d4
Irwin Lee <lihongning_ynnu@126.com>
2015-01-01 18:49:20 -0500

    update main page_position_keep.js

--- page_position_keep.js
+++ page_position_keep.js
@@ -1,6 +1,6 @@
 // This js enable the page to save/load the pageYOffset property to/from document.cookie,
 // and make the page scroll to the last postion when it is shown.
-// usage: <%= stylesheet_link_tag 'page_position_remember' %> in Rails
+// usage: <%= stylesheet_link_tag 'page_position_keep' %> in Rails
 // usage: <script src="/javascripts/page_position_keep.js" type="text/javascript"></script> in Javascript
 
 function getCookie(cname) {
